279ec2f4004a6867c6b646ad485f8236fe1a4f45
Augusto Antunes <augustoantunes@outlook.pt>
2022-08-24 22:45:54 +0100

    Implementação de responsividade na pagina home e criação de pagina ministeria

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 console.log('Teste de funcionamento do WebPack');
 import './assests/style.scss';
 
+import './assests/js/bootstrap.bundle.min.js'
+import './assests/js/popper.min.js'
 import './app/components/nav-menu'
